3e4888dffe220a221bcfd79fc7aa1cce61d4d06d
Ayub Deshmuk <adeshmuk.c@stcs.com.sa>
2017-07-11 07:00:31 +0300

    using just sysout to test utf8

--- src/main/java/com/mkyong/JDBCTestClass.java
+++ src/main/java/com/mkyong/JDBCTestClass.java
@@ -1,57 +1,54 @@
 package com.mkyong;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.nio.charset.Charset;
 
 /**
  * Created by KOUSAR on 7/8/2017.
  */
 public class JDBCTestClass {
     public static void main(String[] args) {
-
-        System.out.println("-------- Oracle JDBC Connection Testing ------");
-
-        try {
-
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-        } catch (ClassNotFoundException e) {
-
-            System.out.println("Where is your Oracle JDBC Driver?");
-            e.printStackTrace();
-            return;
-
-        }
-
-        System.out.println("Oracle JDBC Driver Registered!");
-
-        Connection connection = null;
-
-        try {
-
-            connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@localhost:1521:xe", "jahezdbapp", "oracle");
-            Statement st=connection.createStatement();
-            ResultSet res=st.executeQuery("SELECT menu_item_name from pending_menu_item where menu_item_id=6062");
-
-            while(res.next()){
-                System.out.println("itemName: "+res.getString(1));
-            }
-        } catch (Exception e) {
-
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-        } else {
-            System.out.println("Failed to make connection!");
-        }
+        System.out.println("Default Charset=" + Charset.defaultCharset());
+        System.out.println("الرياض");
+//
+//        try {
+//
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//
+//        } catch (ClassNotFoundException e) {
+//
+//            System.out.println("Where is your Oracle JDBC Driver?");
+//            e.printStackTrace();
+//            return;
+//
+//        }
+//
+//        System.out.println("Oracle JDBC Driver Registered!");
+//
+//        Connection connection = null;
+//
+//        try {
+//
+//            connection = DriverManager.getConnection(
+//                    "jdbc:oracle:thin:@localhost:1521:orcl", "system", "oracledbserviceuser");
+//            Statement st=connection.createStatement();
+//            ResultSet res=st.executeQuery("SELECT city_name_ar from city where city_id=1");
+//
+//            while(res.next()){
+//                System.out.println("itemName: "+res.getString(1));
+//            }
+//        } catch (Exception e) {
+//
+//            System.out.println("Connection Failed! Check output console");
+//            e.printStackTrace();
+//            return;
+//
+//        }
+//
+//        if (connection != null) {
+//            System.out.println("You made it, take control your database now!");
+//        } else {
+//            System.out.println("Failed to make connection!");
+//        }
 
 
     }
--- src/main/java/com/mkyong/SpringBootConsoleApplication.java
+++ src/main/java/com/mkyong/SpringBootConsoleApplication.java
@@ -1,31 +1,14 @@
 package com.mkyong;
 
-import com.mkyong.dao.CustomerRepository;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
-import java.util.List;
+import java.nio.charset.Charset;
 
-import static java.lang.System.exit;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class SpringBootConsoleApplication implements CommandLineRunner {
-
-    @Autowired
-    DataSource dataSource;
-
-    @Autowired
-    private CustomerRepository customerRepository;
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootConsoleApplication.class, args);
@@ -33,23 +16,7 @@ public class SpringBootConsoleApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // If you want to check the HikariDataSource settingsjav
-        HikariDataSource newds = (HikariDataSource)dataSource;
-        System.out.println("getMaximumPoolSize = " + ((HikariDataSource) dataSource).getDataSourceProperties());
-       // newds.getDataSourceProperties().put("useUnicode","yes");
-     //   newds.getDataSourceProperties().put("characterEncoding","UTF-8");
-     //   System.out.println("DATASOURCE = " + newds.getDataSourceProperties().getProperty("hikari.useUnicode"));
-
-        if (args.length <= 0) {
-            System.err.println("[Usage] java xxx.jar {display}");
-        } else {
-                if (args[0].equalsIgnoreCase("display")) {
-                System.out.println("Display items...");
-                List<String> list = customerRepository.findAll();
-                list.forEach(x -> System.out.println(x));
-            }
-            System.out.println("Done!");
-        }
-        exit(0);
+        System.out.println("Default Charset=" + Charset.defaultCharset());
+        System.out.println("test::" +"الرياض");
     }
 }
